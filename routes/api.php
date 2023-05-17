6d195f16dfc7a61f4960ff0793c3d85b81499dd2
AdetonaMichael <powerofelectron616@gmail.com>
2023-05-17 17:30:57 +0100

    created the user api endpoints as well as organized them

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+require __DIR__ .'/api/v1/users.php';
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
